eca74817b863b2342117be93bd4dfd5f1159d4bb
zakizaki0115 <144410051+zakizaki0115@users.noreply.github.com>
2023-11-03 14:58:39 +0700

    Add files via upload

---
+++ theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color accentColor = const Color(0xFF00897b);
+const Color lightAccentColor = const Color(0xFF4ebaaa);
+const Color darkAccentColor = const Color(0xFF005b4f);
